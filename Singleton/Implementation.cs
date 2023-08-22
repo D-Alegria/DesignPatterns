8e9274168d37feb7f78df2a8a298adea51b033b8
Demilade Oladugba <demilade.oladugba@phastmoney.com>
2023-08-22 08:30:48 +0100

    Added singleton pattern

--- Singleton/Implementation.cs
+++ Singleton/Implementation.cs
@@ -1,4 +1,28 @@
 ﻿namespace Singleton
 {
-  
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _lazyLogger = new(() => new Logger());
+        //private static Logger? _instance;
+
+        public static Logger Instance
+        {
+            get
+            {
+                return _lazyLogger.Value;
+                //_instance ??= new Logger();
+                //return _instance;
+            }
+        }
+
+        protected Logger()
+        {
+
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
 }
