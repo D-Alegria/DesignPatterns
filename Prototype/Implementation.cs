841752f0d2c89e0f4cfadf122c5cb17d75634fba
Demilade Oladugba <demilade.oladugba@phastmoney.com>
2023-08-28 09:12:10 +0100

    Added prototype pattern

--- Prototype/Implementation.cs
+++ Prototype/Implementation.cs
@@ -1,4 +1,62 @@
-﻿namespace Prototype
-{
+﻿using Newtonsoft.Json;
 
+namespace Prototype
+{
+    /// <summary>
+    /// Prototype
+    /// </summary>
+    public abstract class Person
+    {
+        public abstract string Name { get; set; }
+
+        public abstract Person Clone(bool deepClone);
+    }
+
+    /// <summary>
+    /// ConcretePrototype
+    /// </summary>
+    public class Manager : Person
+    {
+        public override string Name { get; set; }
+
+        public Manager(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public override Person Clone(bool deepClone = false)
+        {
+            if (deepClone)
+            {
+                var objectAsJson = JsonConvert.SerializeObject(this);
+                return JsonConvert.DeserializeObject<Manager>(objectAsJson)!;
+            }
+            return (Person)MemberwiseClone();
+        }
+    }
+
+    /// <summary>
+    /// ConcretePrototype
+    /// </summary>
+    public class Employee : Person
+    {
+        public Manager Manager { get; set; }
+        public override string Name { get; set; }
+
+        public Employee(string Name, Manager manager)
+        {
+            this.Name = Name;
+            Manager = manager;
+        }
+
+        public override Person Clone(bool deepClone = false)
+        {
+            if (deepClone)
+            {
+                var objectAsJson = JsonConvert.SerializeObject(this);
+                return JsonConvert.DeserializeObject<Employee>(objectAsJson)!;
+            }
+            return (Person)MemberwiseClone();
+        }
+    }
 }
--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.Title = "Prototype";
+﻿using Prototype;
+
+Console.Title = "Prototype";
+
+var manager = new Manager("Demilade");
+var managerClone = (Manager)manager.Clone();
+Console.WriteLine($"Manager was cloned: {managerClone.Name}");
+
+var employee = new Employee("Ismail", managerClone);
+var employeeClone = (Employee)employee.Clone(true);
+Console.WriteLine($"Employee was cloned: {employeeClone.Name}, with manager {employeeClone.Manager.Name}");
+
+// change manager name
+managerClone.Name = "Ibukun";
+Console.WriteLine($"Employee was cloned: {employeeClone.Name}, with manager {employeeClone.Manager.Name}");
+
+Console.ReadKey();
