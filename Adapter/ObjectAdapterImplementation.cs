b831505730dd2b87c2ebe429df580c6853aa5eda
Demilade Oladugba <demilade.oladugba@phastmoney.com>
2023-08-29 04:00:00 +0100

    added Adapter Pattern

---
+++ Adapter/ObjectAdapterImplementation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace Adapter
+{
+	public class CityFromExternalSystem
+	{
+		public string Name;
+		public string NickName;
+		public int Inhabitants;
+
+		public CityFromExternalSystem(string name, string nickName, int inhabitants)
+		{
+			Name = name;
+			NickName = nickName;
+			Inhabitants = inhabitants;
+		}
+	}
+
+	/// <summary>
+	/// Adaptee
+	/// </summary>
+	public class ExternalSystem
+	{
+		public CityFromExternalSystem GetCity()
+		{
+			return new CityFromExternalSystem("Lagos", "LGA", 20000000);
+		}
+    }
+
+	public class City
+	{
+		public string Fullname;
+        public long Inhabitants;
+
+        public City(string fullname, long inhabitants)
+        {
+            Fullname = fullname;
+            Inhabitants = inhabitants;
+        }
+    }
+
+	/// <summary>
+	/// Target
+	/// </summary>
+	public interface ICityAdapter
+	{
+		City GetCity();
+	}
+
+	/// <summary>
+	/// Adapter
+	/// </summary>
+    public class CityAdapter : ICityAdapter
+    {
+		public ExternalSystem ExternalSystem { get; private set; } = new();
+
+        public City GetCity()
+        {
+			var cityFromExternalSystem = ExternalSystem.GetCity();
+			return new City($"{cityFromExternalSystem.Name}-{cityFromExternalSystem.NickName}", cityFromExternalSystem.Inhabitants);
+        }
+    }
+
+}
+
--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.Title = "Adapter";
+﻿using Adapter;
+
+Console.Title = "Adapter";
+
+// Object Adapter
+ICityAdapter adapter = new CityAdapter();
+var city = adapter.GetCity();
+
+Console.WriteLine($"{city.Fullname}, {city.Inhabitants}");
+
+// Class Adapter
+ClassAdapter.ICityAdapter classAdapter = new ClassAdapter.CityAdapter();
+var classAdapterCity = classAdapter.GetCity();
+
+Console.WriteLine($"{classAdapterCity.Fullname}, {classAdapterCity.Inhabitants}");
+Console.ReadKey();
