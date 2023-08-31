60683ea79c1f777db28583977ccfeaebf86311e0
Demilade Oladugba <demilade.oladugba@phastmoney.com>
2023-08-31 14:59:14 +0100

    Added Decorator pattern

--- Decorator/Implementation.cs
+++ Decorator/Implementation.cs
@@ -1,3 +1,94 @@
 ﻿namespace Decorator
 {
+    /// <summary>
+    /// Component (as interface)
+    /// Defines the interface for objects that can have responsibilities
+    /// added to them dynamically
+    /// </summary>
+    public interface IMailService
+    {
+        bool SendMail(string message);
+    }
+
+    /// <summary>
+    /// ConcreteComponent
+    /// This defines an object to which additional responisilities can
+    /// be attached
+    /// </summary>
+    public class OnPremiseMailService : IMailService
+    {
+        public bool SendMail(string message)
+        {
+            Console.WriteLine($"{message} was sent via {nameof(OnPremiseMailService)}");
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// ConcreteComponent
+    /// This defines an object to which additional responisilities can
+    /// be attached
+    /// </summary>
+    public class CloudMailService : IMailService
+    {
+        public bool SendMail(string message)
+        {
+            Console.WriteLine($"{message} was sent via {nameof(CloudMailService)}");
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Decorator
+    /// This maintains a reference to a 'Component' object and defines an
+    /// interface that conforms to 'Components' interface.
+    /// </summary>
+    public abstract class MailServiceDecoratorBase : IMailService
+    {
+        private readonly IMailService _mailService;
+
+        protected MailServiceDecoratorBase(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public virtual bool SendMail(string message)
+        {
+            return _mailService.SendMail(message);
+        }
+    }
+
+    /// <summary>
+    /// Concrete Decorator
+    /// Adds responsibilites to the component
+    /// </summary>
+    public class StatisticsDecorator : MailServiceDecoratorBase
+    {
+        public StatisticsDecorator(IMailService mailService) : base(mailService)
+        {
+        }
+
+        public override bool SendMail(string message)
+        {
+            Console.WriteLine($"Collecting statistics in {nameof(StatisticsDecorator)}");
+            return base.SendMail(message);
+        }
+    }
+
+    /// <summary>
+    /// Concrete Decorator
+    /// Adds responsibilites to the component
+    /// </summary>
+    public class MessageDatabaseDecorator : MailServiceDecoratorBase
+    {
+        public MessageDatabaseDecorator(IMailService mailService) : base(mailService)
+        {
+        }
+
+        public override bool SendMail(string message)
+        {
+            Console.WriteLine($"Messaging Database in {nameof(MessageDatabaseDecorator)}");
+            return base.SendMail(message);
+        }
+    }
 }
--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.Title = "Decorator";
+﻿using Decorator;
+
+Console.Title = "Decorator";
+
+// instantiate mail services
+var cloudMailService = new CloudMailService();
+cloudMailService.SendMail("Hi there, Cloud.");
+
+var onPremiseMailService = new OnPremiseMailService();
+onPremiseMailService.SendMail("Hi there, OnPremise");
+
+// Add More Functionality(Behaviours)
+var statisticsDecorator = new StatisticsDecorator(cloudMailService);
+statisticsDecorator.SendMail($"Hi there via {nameof(StatisticsDecorator)} wrapper");
+
+var messageDatabaseDecorator = new MessageDatabaseDecorator(onPremiseMailService);
+messageDatabaseDecorator.SendMail($"Hi there via {nameof(MessageDatabaseDecorator)} wrapper");
+
+Console.ReadKey();
