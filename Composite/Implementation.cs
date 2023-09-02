b838c43ea7245491bee897051e511ad04979330a
Demilade Oladugba <demilade.oladugba@phastmoney.com>
2023-09-02 10:03:13 +0100

    Added Composite Pattern

--- Composite/Implementation.cs
+++ Composite/Implementation.cs
@@ -1,3 +1,68 @@
 ﻿namespace Composite
 {
+    /// <summary>
+    /// Component
+    /// This declares the interface for objects on the composition and contains
+    /// a common operation
+    /// </summary>
+    public abstract class FileSystemItem
+    {
+        public String Name { get; set; }
+        public abstract long GetSize();
+
+        public FileSystemItem(string name)
+        {
+            Name = name;
+        }
+    }
+
+    /// <summary>
+    /// Leaf
+    /// This represents leaf objects in the composition, and has no children. It
+    /// defines behaviour for primitive objects in the composition.
+    /// </summary>
+    public class File : FileSystemItem
+    {
+        private long _size;
+        public File(string name, long size) : base(name)
+        {
+            _size = size;
+        }
+
+        public override long GetSize()
+        {
+            return _size;
+        }
+    }
+
+    /// <summary>
+    /// Composite
+    /// This stores child components and defines behaviour for components having
+    /// children.
+    /// </summary>
+    public class Directory : FileSystemItem
+    {
+        private long _size;
+        private List<FileSystemItem> _items = new();
+
+        public Directory(string name,long size) : base(name)
+        {
+            _size = size;
+        }
+
+        public override long GetSize()
+        {
+            return _size + _items.Select(item => item.GetSize()).Sum();
+        }
+
+        public void AddFileSystemItem(FileSystemItem file)
+        {
+            _items.Add(file);
+        }
+
+        public void RemoveFileSystemItem(FileSystemItem file)
+        {
+            _items.Remove(file);
+        }
+    }
 }
--- Composite/Program.cs
+++ Composite/Program.cs
@@ -1,1 +1,22 @@
 ﻿Console.Title = "Composite";
+
+var root = new Composite.Directory("root", 0);
+var topLevelFile = new Composite.File("toplevel.txt", 100);
+var topLevelDirectory1 = new Composite.Directory("topLevelDirectory1", 4);
+var topLevelDirectory2 = new Composite.Directory("topLevelDirectory2", 4);
+
+root.AddFileSystemItem(topLevelFile);
+root.AddFileSystemItem(topLevelDirectory1);
+root.AddFileSystemItem(topLevelDirectory2);
+
+var subLevelFile1 = new Composite.File("sublevel1.txt", 200);
+var subLevelFile2 = new Composite.File("sublevel2.txt", 150);
+
+topLevelDirectory2.AddFileSystemItem(subLevelFile1);
+topLevelDirectory2.AddFileSystemItem(subLevelFile2);
+
+Console.WriteLine($"Size of topLevelDirectory1: {topLevelDirectory1.GetSize()}");
+Console.WriteLine($"Size of topLevelDirectory2: {topLevelDirectory2.GetSize()}");
+Console.WriteLine($"Size of root: {root.GetSize()}");
+
+Console.ReadKey();
