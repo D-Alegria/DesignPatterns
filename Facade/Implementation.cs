32f61bc4b232b313a74cf4155b4bc513182e9e3e
Demilade Oladugba <demilade.oladugba@phastmoney.com>
2023-09-03 07:51:03 +0100

    Added Facade Pattern

--- Facade/Implementation.cs
+++ Facade/Implementation.cs
@@ -1,3 +1,61 @@
 ﻿namespace Facade
 {
+    /// <summary>
+    /// Facade
+    /// This knows which subsystem classes are responsible for a request, and a
+    /// delegates client requests to appropriate subsystem objects.
+    /// </summary>
+    public class DiscountFacade
+    {
+        private readonly OrderService _orderService = new();
+        private readonly CustomerDiscountBaseService _customerDiscountBaseService = new();
+        private readonly DayOfTheWeekFactorService _dayOfTheWeekFactorService = new();
+
+        public double CalculateDiscount(int customerId)
+        {
+            if (!_orderService.HasEnoughOrders(customerId))
+            {
+                return 0;
+            }
+
+            return _customerDiscountBaseService.CalculateDiscountBase(customerId) * _dayOfTheWeekFactorService.CalculateDayOfTheWeekFactor();
+        }
+    }
+
+    /// <summary>
+    /// Subsystem Class
+    /// </summary>
+    public class OrderService
+    {
+        public bool HasEnoughOrders(int customerId)
+        {
+            return customerId > 5;
+        }
+    }
+
+    /// <summary>
+    /// Subsystem Class
+    /// </summary>
+    public class CustomerDiscountBaseService
+    {
+        public double CalculateDiscountBase(int customerId)
+        {
+            return (customerId > 8) ? 10 : 20;
+        }
+    }
+
+    /// <summary>
+    /// Subsystem Class
+    /// </summary>
+    public class DayOfTheWeekFactorService
+    {
+        public double CalculateDayOfTheWeekFactor()
+        {
+            return DateTime.UtcNow.DayOfWeek switch
+            {
+                DayOfWeek.Sunday or DayOfWeek.Saturday => 0.8,
+                _ => 1.2,
+            };
+        }
+    }
 }
--- Facade/Program.cs
+++ Facade/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.Title = "Facade";
+﻿using Facade;
+
+Console.Title = "Facade";
+
+var facade = new DiscountFacade();
+Console.WriteLine($"Discount percentage foe customer with id 1: {facade.CalculateDiscount(1)}");
+Console.WriteLine($"Discount percentage foe customer with id 10: {facade.CalculateDiscount(10)}");
+
+Console.ReadKey();
