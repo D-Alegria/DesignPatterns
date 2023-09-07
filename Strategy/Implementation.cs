411973285b7f5985992b3604349010a07d345019
Demilade Oladugba <demilade.oladugba@phastmoney.com>
2023-09-07 08:59:21 +0100

    Added strategy pattern

--- Strategy/Implementation.cs
+++ Strategy/Implementation.cs
@@ -1,3 +1,101 @@
 ﻿namespace Strategy
 {
+    /// <summary>
+    /// Strategy
+    /// This declares an interface common to all supported algorithms.
+    /// CONTEXT uses this interface to call the algorithm defined by a
+    /// CONCRETE STRATEGY
+    /// </summary>
+    public interface IExportService
+    {
+        void Export(Order order);
+    }
+
+    /// <summary>
+    /// Concrete Strategy
+    /// This an implementation of the STRATEGY interface.
+    /// It also houses the algorithms for the STRATEGY
+    /// </summary>
+    public class CSVExportServce : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} to CSV");
+        }
+    }
+
+    // <summary>
+    /// Concrete Strategy
+    /// This an implementation of the STRATEGY interface.
+    /// It also houses the algorithms for the STRATEGY
+    /// </summary>
+    public class XMLExportServce : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} to XML");
+        }
+    }
+
+    // <summary>
+    /// Concrete Strategy
+    /// This an implementation of the STRATEGY interface.
+    /// It also houses the algorithms for the STRATEGY
+    /// </summary>
+    public class JSONExportServce : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} to JSON");
+        }
+    }
+
+    /// <summary>
+    /// Context
+    /// This is configured with a CONCRETE STRATEGY object and maintains a
+    /// reference to a STRATEGY object()
+    /// </summary>
+    public class Order
+    {
+        public IExportService? ExportService { get; set; }
+        public string Name { get; set; }
+        public string Customer { get; set; }
+        public int Amount { get; set; }
+        public string? Description { get; set; }
+
+        public Order(string customer, string name, int amount)
+        {
+            Customer = customer;
+            Name = name;
+            Amount = amount;
+        }
+
+        public void Export()
+        {
+            ExportService?.Export(this);
+        }
+    }
+
+    /// <summary>
+    /// Context - Strategy Pattern Variation with Method Parameter
+    /// This is configured with a CONCRETE STRATEGY object and maintains a
+    /// reference to a STRATEGY object()
+    /// </summary>
+    public class OrderWithMethodParameter : Order
+    {
+
+        public OrderWithMethodParameter(string customer, string name, int amount) : base(customer, name, amount)
+        {
+        }
+
+        public void Export(IExportService exportService)
+        {
+            if (exportService is null)
+            {
+                throw new ArgumentNullException(nameof(exportService));
+            }
+
+            exportService?.Export(this);
+        }
+    }
 }
--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.Title = "Strategy";
+﻿using Strategy;
+
+Console.Title = "Strategy";
+
+var order = new Order("Demilade", "Visual Studio License", 5);
+order.ExportService = new CSVExportServce();
+order.Export();
+
+order.ExportService = new JSONExportServce();
+order.Export();
+
+var orderWithMethodParameter = new OrderWithMethodParameter("Demilade", "Visual Studio License", 5);
+orderWithMethodParameter.Export(new CSVExportServce());
+
+orderWithMethodParameter.Export(new JSONExportServce());
+
+Console.ReadKey();
