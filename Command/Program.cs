0ebe3d12511b499d33c94139d33fe76690d391c2
Demilade Oladugba <demilade.oladugba@phastmoney.com>
2023-09-08 09:12:02 +0100

    Added Command Pattern

--- Command/Program.cs
+++ Command/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.Title = "Command";
+﻿using Command;
+
+Console.Title = "Command";
+
+CommandManager commandManager = new();
+IEmployeeManagerRepository repository = new EmployeeManagetRepository();
+
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 1, new Employee(111, "Toby")));
+repository.WriteToDataStore();
+
+commandManager.Undo();
+repository.WriteToDataStore();
+
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 1, new Employee(222, "Clara")));
+repository.WriteToDataStore();
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 2, new Employee(333, "Tom")));
+repository.WriteToDataStore();
+
+// Adding the same employee twice
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 2, new Employee(333, "Tom")));
+repository.WriteToDataStore();
+
+commandManager.UndoAll();
+repository.WriteToDataStore();
+
+Console.ReadKey();
