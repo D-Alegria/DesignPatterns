5d49decea8c460e4e036b8dbccafc406d0771825
Demilade Oladugba <demilade.oladugba@phastmoney.com>
2023-09-11 09:14:02 +0100

    Added Mediator Pattern

--- Mediator/Implementation.cs
+++ Mediator/Implementation.cs
@@ -1,4 +1,127 @@
 ﻿namespace Mediator
 {
+    /// <summary>
+    /// Mediator
+    /// This defines an interface for communicating with Colleague objects
+    /// </summary>
+    public interface IChatRoom
+    {
+        void Register(TeamMember teamMember);
+        void Send(string from, string message);
+        void Send(string from, string to, string message);
+        void SendTo<T>(string from, string message) where T : TeamMember;
+    }
+
+    /// <summary>
+    /// ConcreteMediator knows and maintains it's colleagues, and it implements
+    /// cooperative behaviour by coordinating Colleague objects
+    /// </summary>
+    public class TeamChatRoom : IChatRoom
+    {
+        private readonly Dictionary<string, TeamMember> teamMembers = new();
+        public void Register(TeamMember teamMember)
+        {
+            teamMember.SetChatRoom(this);
+            if (!teamMembers.ContainsKey(teamMember.Name))
+            {
+                teamMembers.Add(teamMember.Name, teamMember);
+            }
+        }
+
+        public void Send(string from, string message)
+        {
+            foreach (var teamMember in teamMembers.Values)
+            {
+                teamMember.Receive(from, message);
+            }
+        }
+
+        public void Send(string from, string to, string message)
+        {
+            var teamMember = teamMembers[to];
+            teamMember?.Receive(from, message);
+        }
+
+        public void SendTo<T>(string from, string message) where T : TeamMember
+        {
+            foreach (var teamMember in teamMembers.Values.OfType<T>())
+            {
+                teamMember.Receive(from, message);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Colleague
+    /// This knows the Mediator and communicated with it instead of with another
+    /// Colleague
+    /// </summary>
+    public abstract class TeamMember
+    {
+        private IChatRoom? _chatRoom;
+        public string Name { get; set; }
+
+        public TeamMember(string name)
+        {
+            Name = name;
+        }
+
+        internal void SetChatRoom(IChatRoom chatRoom)
+        {
+            _chatRoom = chatRoom;
+        }
+
+        public void SendTo<T>(string message) where T : TeamMember
+        {
+            _chatRoom?.SendTo<T>(Name, message);
+        }
+
+        public void Send(string to, string message)
+        {
+            _chatRoom?.Send(Name, to, message);
+        }
+
+        public void Send(string message)
+        {
+            _chatRoom?.Send(Name, message);
+        }
+
+        public virtual void Receive(string from, string message)
+        {
+            Console.WriteLine($"Message {from} to {Name} : {message}");
+        }
+    }
+
+    /// <summary>
+    /// ConcreteColleague
+    /// </summary>
+    public class AccountManager : TeamMember
+    {
+        public AccountManager(string name) : base(name)
+        {
+        }
+
+        public override void Receive(string from, string message)
+        {
+            Console.WriteLine($"{nameof(AccountManager)} {Name} received: ");
+            base.Receive(from, message);
+        }
+    }
+
+    /// <summary>
+    /// ConcreteColleague
+    /// </summary>
+    public class Lawyer : TeamMember
+    {
+        public Lawyer(string name) : base(name)
+        {
+        }
+
+        public override void Receive(string from, string message)
+        {
+            Console.WriteLine($"{nameof(Lawyer)} {Name} received: ");
+            base.Receive(from, message);
+        }
+    }
 }
  
--- Mediator/Program.cs
+++ Mediator/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.Title = "Mediator";
+﻿using Mediator;
+
+Console.Title = "Mediator";
+
+TeamChatRoom teamChatRoom = new();
+
+var demi = new AccountManager("Demi");
+var temi = new AccountManager("Temi");
+var lashe = new AccountManager("Lashe");
+var victor = new Lawyer("Victor");
+var Dubem = new Lawyer("Dubem");
+
+teamChatRoom.Register(demi);
+teamChatRoom.Register(temi);
+teamChatRoom.Register(lashe);
+teamChatRoom.Register(victor);
+teamChatRoom.Register(Dubem);
+
+demi.Send("Hi everyone, who would love to go on a trip to Ibiza next year");
+victor.Send("Sure on it");
+Dubem.Send("Temi","Can we hop on a call");
+Dubem.Send("Demi", "All good to go");
+demi.SendTo<AccountManager>("Tickets should be purchased");
+
+Console.ReadKey();
