cbca06e15891d37f38899fb928a07f4bb70f6a71
Demilade Oladugba <demilade.oladugba@phastmoney.com>
2023-09-12 09:13:38 +0100

    Added chain of responsibility pattern

--- ChainOfResponsibility/Implementation.cs
+++ ChainOfResponsibility/Implementation.cs
@@ -1,3 +1,137 @@
-﻿namespace ChainOfResponsibility
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChainOfResponsibility
 {
+    /// <summary>
+    /// Handler
+    /// This defines an interface for handling requests, and optionally
+    /// implements the successor link
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IHandler<T> where T : class
+    {
+        void Handle(T request);
+        IHandler<T> SetSuccessor(IHandler<T> successor);
+    }
+
+    /// <summary>
+    /// ConcreteHandler
+    /// This handle requests it's responsible for. It can access the successor
+    /// and potentially pass the request on.
+    /// </summary>
+    /// <typeparam name="Document"></typeparam>
+    public class DocumentTitleHandler : IHandler<Document>
+    {
+
+        private IHandler<Document>? _successor;
+        public void Handle(Document request)
+        {
+            if (request.Title == string.Empty)
+            {
+                throw new ValidationException(new ValidationResult("Title must be filled out", new List<string>() { "Title" }), null, null);
+            }
+            // go to the next handler
+            _successor?.Handle(request);
+        }
+
+        public IHandler<Document> SetSuccessor(IHandler<Document> successor)
+        {
+            _successor = successor;
+            return successor;
+        }
+    }
+
+    /// <summary>
+    /// ConcreteHandler
+    /// This handle requests it's responsible for. It can access the successor
+    /// and potentially pass the request on.
+    /// </summary>
+    /// <typeparam name="Document"></typeparam>
+    public class DocumentLastModifiedHandler : IHandler<Document>
+    {
+        private IHandler<Document>? _successor;
+        public void Handle(Document request)
+        {
+            if (request.LastModified < DateTime.UtcNow.AddDays(-30))
+            {
+                throw new ValidationException(new ValidationResult("Document must be modified in the last 30 days", new List<string>() { "LastModified" }), null, null);
+            }
+            // go to the next handler
+            _successor?.Handle(request);
+        }
+
+        public IHandler<Document> SetSuccessor(IHandler<Document> successor)
+        {
+            _successor = successor;
+            return successor;
+        }
+    }
+
+    /// <summary>
+    /// ConcreteHandler
+    /// This handle requests it's responsible for. It can access the successor
+    /// and potentially pass the request on.
+    /// </summary>
+    /// <typeparam name="Document"></typeparam>
+    public class DocumentApprovedByLitigationHandler : IHandler<Document>
+    {
+        private IHandler<Document>? _successor;
+        public void Handle(Document request)
+        {
+            if (!request.ApprovedByLitigation)
+            {
+                throw new ValidationException(new ValidationResult("Document must be approved by litigation", new List<string>() { "ApprovedByLitigation" }), null, null);
+            }
+            // go to the next handler
+            _successor?.Handle(request);
+        }
+
+        public IHandler<Document> SetSuccessor(IHandler<Document> successor)
+        {
+            _successor = successor;
+            return successor;
+        }
+    }
+
+    /// <summary>
+    /// ConcreteHandler
+    /// This handle requests it's responsible for. It can access the successor
+    /// and potentially pass the request on.
+    /// </summary>
+    /// <typeparam name="Document"></typeparam>
+    public class DocumentApprovedByManagementHandler : IHandler<Document>
+    {
+        private IHandler<Document>? _successor;
+        public void Handle(Document request)
+        {
+            if (!request.ApprovedByManagement)
+            {
+                throw new ValidationException(new ValidationResult("Document must be approved by managment", new List<string>() { "ApprovedByManagement" }), null, null);
+            }
+            // go to the next handler
+            _successor?.Handle(request);
+        }
+
+        public IHandler<Document> SetSuccessor(IHandler<Document> successor)
+        {
+            _successor = successor;
+            return successor;
+        }
+    }
+
+    public class Document
+    {
+        public string Title { get; set; }
+        public DateTimeOffset LastModified { get; set; }
+        public bool ApprovedByLitigation { get; set; }
+        public bool ApprovedByManagement { get; set; }
+
+        public Document(string title, DateTimeOffset lastModified, bool approvedByLitigation, bool approvedByManagement)
+        {
+            Title = title;
+            LastModified = lastModified;
+            ApprovedByLitigation = approvedByLitigation;
+            ApprovedByManagement = approvedByManagement;
+        }
+    }
 }
--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.Title = "Chain of Responsibility";
+﻿using System.ComponentModel.DataAnnotations;
+using ChainOfResponsibility;
+
+Console.Title = "Chain of Responsibility";
+
+var validDocument = new Document("How to Avoid Java Development", DateTimeOffset.UtcNow, true, true);
+var inValidDocument = new Document("How to Avoid Java Development", DateTimeOffset.UtcNow, false, true);
+
+var documentHandlerChain = new DocumentTitleHandler();
+documentHandlerChain
+    .SetSuccessor(new DocumentLastModifiedHandler())
+    .SetSuccessor(new DocumentApprovedByLitigationHandler())
+    .SetSuccessor(new DocumentApprovedByManagementHandler());
+
+
+try
+{
+    documentHandlerChain.Handle(validDocument);
+    Console.WriteLine("Valid document is valid.");
+    documentHandlerChain.Handle(inValidDocument);
+    Console.WriteLine("Invalid document is valid.");
+}
+catch (ValidationException validationException)
+{
+    Console.WriteLine(validationException.Message);
+}
+
+Console.ReadKey();
