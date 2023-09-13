9fd286a4d3ecb1d0740461a6158245bf2856ca0f
Demilade Oladugba <demilade.oladugba@phastmoney.com>
2023-09-13 09:01:08 +0100

    Addded Observer pattern

--- Observer/Implementation.cs
+++ Observer/Implementation.cs
@@ -1,3 +1,99 @@
 ﻿namespace Observer
 {
+    public class TicketChange
+    {
+        public int Amount { get; private set; }
+        public int ArtistId { get; private set; }
+
+        public TicketChange(int amount, int artistId)
+        {
+            Amount = amount;
+            ArtistId = artistId;
+        }
+    }
+
+    /// <summary>
+    /// Observer
+    /// This defines an updating interface for objects that should be notified
+    /// of changes in a Subject
+    /// </summary>
+    public interface ITicketChangeListener
+    {
+        void ReceiveTicketChangeNotification(TicketChange ticketChange);
+    }
+
+    /// <summary>
+    /// ConcreteObserver
+    /// This store state that must remain consistent with the Subjects state.
+    /// They implement the Observer updating interface to keep state consistent.
+    /// </summary>
+    public class TicketStockService : ITicketChangeListener
+    {
+        public void ReceiveTicketChangeNotification(TicketChange ticketChange)
+        {
+            Console.WriteLine($"{nameof(TicketStockService)} notified of " +
+              $"ticket change: artist {ticketChange.ArtistId}, " +
+              $"amount {ticketChange.Amount}");
+        }
+    }
+
+    /// <summary>
+    /// ConcreteObserver
+    /// This store state that must remain consistent with the Subjects state.
+    /// They implement the Observer updating interface to keep state consistent.
+    /// </summary>
+    public class TicketResellerService : ITicketChangeListener
+    {
+        public void ReceiveTicketChangeNotification(TicketChange ticketChange)
+        {
+            Console.WriteLine($"{nameof(TicketResellerService)} notified of " +
+                $"ticket change: artist {ticketChange.ArtistId}, " +
+                $"amount {ticketChange.Amount}");
+        }
+    }
+
+    /// <summary>
+    /// Subject
+    /// This knows it's Observers. It provides an interface for attaching and
+    /// detaching them.
+    /// </summary>
+    public abstract class TicketChangeNotifier
+    {
+        private List<ITicketChangeListener> _observers = new();
+
+        public void AddObserver(ITicketChangeListener observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void RemoveObserver(ITicketChangeListener observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify(TicketChange ticketChange)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.ReceiveTicketChangeNotification(ticketChange);
+            }
+        }
+    }
+
+    /// <summary>
+    /// ConcreteSubject
+    /// This stores state of interest to ConcreteObserver objects, and sends a
+    /// notification to its Observers when it's state changes.
+    /// </summary>
+    public class OrderService: TicketChangeNotifier
+    {
+        public void CompleteTicketSale(int artistId, int amount)
+        {
+            // Mimicking DB Update
+            Console.WriteLine($"{nameof(OrderService)} is changing it's state.");
+            Console.WriteLine($"{nameof(OrderService)} is notifying observers...");
+            Notify(new TicketChange(artistId, amount));
+
+        }
+    }
 }
--- Observer/Program.cs
+++ Observer/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.Title = "Observer";
+﻿using Observer;
+
+Console.Title = "Observer";
+
+TicketStockService ticketStockService = new();
+TicketResellerService ticketResellerService = new();
+OrderService orderService = new();
+
+// add observers
+orderService.AddObserver(ticketStockService);
+orderService.AddObserver(ticketResellerService);
+
+// notify
+orderService.CompleteTicketSale(1, 2);
+
+Console.WriteLine();
+
+// remove one observer
+orderService.RemoveObserver(ticketResellerService);
+
+// notify
+orderService.CompleteTicketSale(2, 4);
+
+Console.ReadKey();
