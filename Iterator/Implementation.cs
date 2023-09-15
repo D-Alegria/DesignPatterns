bc6d64c6616143c69282efc1f6a5f2764e0484ba
Demilade Oladugba <demilade.oladugba@phastmoney.com>
2023-09-15 10:09:31 +0100

    Added Iterator Pattern

--- Iterator/Implementation.cs
+++ Iterator/Implementation.cs
@@ -1,3 +1,96 @@
 ﻿namespace Iterator
 {
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public Person(string name, string country)
+        {
+            Name = name;
+            Country = country;
+        }
+    }
+
+    /// <summary>
+    /// Iterator
+    /// This defines an interface for accessing and traversing elements.
+    /// </summary>
+    public interface IPeopleIterator
+    {
+        Person First();
+        Person Next();
+        bool IsDone { get; }
+        Person CurrentItem { get; }
+    }
+
+    /// <summary>
+    /// ConcreteIterator
+    /// This implements the Iterator interface and keeps track of the current
+    /// position of the Aggregate
+    /// </summary>
+    public class PeopleIterator : IPeopleIterator
+    {
+        private PeopleCollection _peopleCollection;
+        private int _currentItem;
+
+        public PeopleIterator(PeopleCollection peopleCollection)
+        {
+            _peopleCollection = peopleCollection;
+        }
+
+        public bool IsDone
+        {
+            get
+            {
+                return _currentItem >= _peopleCollection.Count;
+            }
+        }
+
+        public Person CurrentItem
+        {
+            get
+            {
+                return _peopleCollection.OrderBy(p => p.Name).ToList()[_currentItem];
+            }
+        }
+
+        public Person First()
+        {
+            _currentItem = 0;
+            return _peopleCollection.OrderBy(p => p.Name).ToList()[_currentItem];
+        }
+
+        public Person Next()
+        {
+            _currentItem++;
+            if (!IsDone)
+                return _peopleCollection.OrderBy(p => p.Name).ToList()[_currentItem];
+            else
+                return null;
+        }
+    }
+
+    /// <summary>
+    /// Aggregate
+    /// This defines an interface for creating an iterator object.
+    /// </summary>
+    public interface IPeopleCollection
+    {
+        IPeopleIterator CreateIterator();
+    }
+
+    /// <summary>
+    /// ConcreteAggregate
+    /// This implements the Iterator creation interface to return an instance of
+    /// the proper ConcreteIterator.
+    /// </summary>
+    public class PeopleCollection : List<Person>, IPeopleCollection
+    { 
+
+        public IPeopleIterator CreateIterator()
+        {
+            return new PeopleIterator(this);
+        }
+    }
 }
--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.Title = "Iterator";
+﻿using Iterator;
+
+Console.Title = "Iterator";
+
+// Create the collection
+PeopleCollection people = new()
+{
+    new Person("Demilade Oladugba", "Nigeria"), 
+    new Person("Olumide Ogundare","Nigeria"),
+    new Person("Abubakar Yusuf", "Nigeria"),
+    new Person("Amit Dwivedi", "India"),
+    new Person("Jacquelina Garrido", "Argentina")
+};
+
+// Create the Iterator
+var peopleIterator = people.CreateIterator();
+
+for (Person person = peopleIterator.First(); !peopleIterator.IsDone; person = peopleIterator.Next())
+{
+    Console.WriteLine(person?.Name);
+}
+
+Console.ReadKey();
